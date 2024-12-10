cc3c9799ea6c2281feccb5a8b8a29754fdfc6165
Torrente178 <jfernandez.fpsalceda@gmail.com>
2024-12-11 00:29:48 +0100

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar() {
   return (
     <div className="w-full flex text-green-300 items-center relative">
       <img src="/logo.png" alt="" className="w-8 h-8 mr-2" />
-      <p className="text-4xl font-medium text-green-300">GPRM</p>
+      <p className="text-4xl font-medium text-green-300">Esto es una Prueba</p>
       <a
         href="https://www.buymeacoffee.com/VishwaGauravIn"
         className="flex absolute right-0 justify-center items-center p-2 px-6 bg-green-300 text-green-900 font-semibold rounded-full shadow-lg hover:shadow-green-300/30 sm:hover:scale-[1.02] transition-all ease-in-out duration-100 scale-75 -mr-6 md:mr-0 sm:scale-100"
